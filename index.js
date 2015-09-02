3547ef5056b77ec9a9e6c7724439c21daabf13da
inuscript <inuscript@gmail.com>
2015-09-02 21:54:38 +0900

    refactor

--- index.js
+++ index.js
@@ -8,20 +8,16 @@ var average = function(arr) {
 }
 
 module.exports = function(colors){
-  var r = []
-  var g = []
-  var b = []
+  var clrs = [[],[],[]]
   var cs = colors.map(function(clr){
     var rgb = Color(clr).rgbArray()
-    r.push(rgb[0])
-    g.push(rgb[1])
-    b.push(rgb[2])
+    rgb.forEach(function(v, i){
+      clrs[i].push(v)
+    })
   })
-  return Color().rgb(average(r), average(g), average(b)).hexString()
-  return colors.reduce(function(mixed, clr){
-    if(mixed === null){
-      return Color(clr)
-    }
-    return mixed.mix(Color(clr))
-  }, null).hexString()
+  return Color().rgb(
+    average(clrs[0]), // r
+    average(clrs[1]), // g
+    average(clrs[2])  // b
+  ).hexString()
 }
